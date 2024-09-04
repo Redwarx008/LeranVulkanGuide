e7602f495dcdb99a061a9cc99fc01d73b7014fb4
Redwarx008 <50837890+Redwarx008@users.noreply.github.com>
2024-09-05 02:22:22 +0800

    init

---
+++ src/vk_engine.cpp
@@ -0,0 +1,51 @@
+﻿
+#include "vk_engine.h"
+
+
+#include <vk_types.h>
+#include <vk_initializers.h>
+#include <iostream>
+#include <format>
+
+void VulkanEngine::init()
+{
+	if (!glfwInit()) {
+		std::cout << std::format("[ InitializeWindow ] ERROR\nFailed to initialize GLFW!\n");
+		return;
+	}
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	//glfwWindowHint(GLFW_RESIZABLE, isResizable);
+	pWindow = glfwCreateWindow(_windowExtent.width, _windowExtent.height, "LeranVulkan", nullptr, nullptr);
+	if (!pWindow) {
+		std::cout << std::format("[ InitializeWindow ]\nFailed to create a glfw window!\n");
+		glfwTerminate();
+		return;
+	}
+	//everything went fine
+	_isInitialized = true;
+}
+void VulkanEngine::cleanup()
+{	
+	if (_isInitialized) {
+		
+		glfwTerminate();
+	}
+}
+
+void VulkanEngine::draw()
+{
+	//nothing yet
+}
+
+void VulkanEngine::run()
+{
+	while (!glfwWindowShouldClose(pWindow)) 
+	{
+
+		/*渲染过程，待填充*/
+
+		glfwPollEvents();
+		draw();
+	}
+}
+
---
+++ src/vk_engine.h
@@ -0,0 +1,32 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	GLFWwindow* pWindow;
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+};
