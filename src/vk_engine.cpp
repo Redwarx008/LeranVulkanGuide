519e875bf1d2fc65408a9cee82c9ae2132cdcce7
Redwarx008 <redwarx008@outlook.com>
2024-09-05 18:43:33 +0800

    day1

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,11 +1,25 @@
 ﻿
 #include "vk_engine.h"
 
-
 #include <vk_types.h>
 #include <vk_initializers.h>
+
+//bootstrap library
+#include "VkBootstrap.h"
+
 #include <iostream>
 #include <format>
+
+#define VK_CHECK(x)                                                 \
+	do                                                              \
+	{                                                               \
+		VkResult err = x;                                           \
+		if (err)                                                    \
+		{                                                           \
+			std::cout <<"Detected Vulkan error: " << err << std::endl; \
+			abort();                                                \
+		}                                                           \
+	} while (0)
 
 void VulkanEngine::init()
 {
@@ -15,12 +29,24 @@ void VulkanEngine::init()
 	}
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	//glfwWindowHint(GLFW_RESIZABLE, isResizable);
-	pWindow = glfwCreateWindow(_windowExtent.width, _windowExtent.height, "LeranVulkan", nullptr, nullptr);
-	if (!pWindow) {
+	_window = glfwCreateWindow(_windowExtent.width, _windowExtent.height, "LeranVulkan", nullptr, nullptr);
+	if (!_window) {
 		std::cout << std::format("[ InitializeWindow ]\nFailed to create a glfw window!\n");
 		glfwTerminate();
 		return;
 	}
+
+	//load the core Vulkan structures
+	init_vulkan();
+
+	//create the swapchain
+	init_swapchain();
+
+	init_commands();
+
+	init_default_renderpass();
+
+	init_framebuffers();
 	//everything went fine
 	_isInitialized = true;
 }
@@ -28,6 +54,21 @@ void VulkanEngine::cleanup()
 {	
 	if (_isInitialized) {
 		
+		vkDestroyCommandPool(_device, _commandPool, nullptr);
+
+		vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+		//destroy swapchain resources
+		for (int i = 0; i < _swapchainImageViews.size(); i++) {
+
+			vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+		}
+
+		vkDestroyDevice(_device, nullptr);
+		vkDestroySurfaceKHR(_instance, _surface, nullptr);
+		vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+		vkDestroyInstance(_instance, nullptr);
+
 		glfwTerminate();
 	}
 }
@@ -39,7 +80,7 @@ void VulkanEngine::draw()
 
 void VulkanEngine::run()
 {
-	while (!glfwWindowShouldClose(pWindow)) 
+	while (!glfwWindowShouldClose(_window)) 
 	{
 
 		/*渲染过程，待填充*/
@@ -49,3 +90,100 @@ void VulkanEngine::run()
 	}
 }
 
+void VulkanEngine::init_vulkan()
+{
+	vkb::InstanceBuilder builder;
+
+	//make the Vulkan instance, with basic debug features
+	auto inst_ret = builder.set_app_name("Example Vulkan Application")
+		.request_validation_layers(true)
+		.require_api_version(1, 1, 0)
+		.use_default_debug_messenger()
+		.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	//store the instance
+	_instance = vkb_inst.instance;
+	//store the debug messenger
+	_debug_messenger = vkb_inst.debug_messenger;
+
+	glfwCreateWindowSurface(_instance, _window, nullptr, &_surface);
+
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 1)
+		.set_surface(_surface)
+		.select()
+		.value();
+
+	//create the final Vulkan device
+	vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the VkDevice handle used in the rest of a Vulkan application
+	_device = vkbDevice.device;
+	_chosenGPU = physicalDevice.physical_device;
+
+	// use vkbootstrap to get a Graphics queue
+	_graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+	_graphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+}
+
+void VulkanEngine::init_swapchain()
+{
+	vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU,_device,_surface };
+
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+		.use_default_format_selection()
+		//use vsync present mode
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+		.set_desired_extent(_windowExtent.width, _windowExtent.height)
+		.build()
+		.value();
+
+	//store swapchain and its related images
+	_swapchain = vkbSwapchain.swapchain;
+	_swapchainImages = vkbSwapchain.get_images().value();
+	_swapchainImageViews = vkbSwapchain.get_image_views().value();
+
+	_swapchainImageFormat = vkbSwapchain.image_format;
+}
+
+void VulkanEngine::init_commands()
+{
+	//create a command pool for commands submitted to the graphics queue.
+	//we also want the pool to allow for resetting of individual command buffers
+	VkCommandPoolCreateInfo commandPoolInfo = vkinit::command_pool_create_info(_graphicsQueueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+
+	VK_CHECK(vkCreateCommandPool(_device, &commandPoolInfo, nullptr, &_commandPool));
+
+	//allocate the default command buffer that we will use for rendering
+	VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::command_buffer_allocate_info(_commandPool, 1);
+
+	VK_CHECK(vkAllocateCommandBuffers(_device, &cmdAllocInfo, &_mainCommandBuffer));
+}
+
+void VulkanEngine::init_default_renderpass()
+{
+	// the renderpass will use this color attachment.
+	VkAttachmentDescription color_attachment = {};
+	//the attachment will have the format needed by the swapchain
+	color_attachment.format = _swapchainImageFormat;
+	//1 sample, we won't be doing MSAA
+	color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	// we Clear when this attachment is loaded
+	color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	// we keep the attachment stored when the renderpass ends
+	color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	//we don't care about stencil
+	color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+	//we don't know or care about the starting layout of the attachment
+	color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+
+	//after the renderpass ends, the image has to be on a layout ready for display
+	color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+}
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -7,6 +7,7 @@
 #include <GLFW/glfw3.h>
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
@@ -16,7 +17,34 @@ public:
 
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
-	GLFWwindow* pWindow;
+	GLFWwindow* _window;
+
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain; // from other articles
+
+	// image format expected by the windowing system
+	VkFormat _swapchainImageFormat;
+
+	//array of images from the swapchain
+	std::vector<VkImage> _swapchainImages;
+
+	//array of image-views from the swapchain
+	std::vector<VkImageView> _swapchainImageViews;
+
+	VkQueue _graphicsQueue; //queue we will submit to
+	uint32_t _graphicsQueueFamily; //family of that queue
+
+	VkCommandPool _commandPool; //the command pool for our commands
+	VkCommandBuffer _mainCommandBuffer; //the buffer we will record into
+
+	VkRenderPass _renderPass;
+
+	std::vector<VkFramebuffer> _framebuffers;
 
 	//initializes everything in the engine
 	void init();
@@ -29,4 +57,10 @@ public:
 
 	//run main loop
 	void run();
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_default_renderpass();
+	void init_framebuffers();
 };
